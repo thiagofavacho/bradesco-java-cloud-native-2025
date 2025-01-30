4245974680228358bbcd127fa8f0f2d60a04df19
Thiago Favacho R. <thiagofr89@hotmail.com>
2025-01-30 16:11:23 -0300

    Initial commit

---
+++ src/test/java/edu/thiago/api_01/BradescoJavaCloudNative2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.thiago.api_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoJavaCloudNative2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
